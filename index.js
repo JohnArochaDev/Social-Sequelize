d05249c7610249e4c99aee4099d5966bb255cc8e
John Arocha <john.arocha@clover.com>
2024-11-06 12:57:49 -0700

    working on a bug with imports

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { Comment, Like, Post, Profile, User } = require("./models/index");
 
-// Define your associations here
-
-
-
-
+User.hasOne(Profile);
+Profile.belongsTo(User);
+User.hasMany(Post);
+Post.hasOne(User);
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+User.hasMany(Like);
+Like.hasMany(User);
 
 module.exports = {
     Comment,
--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const { Comment, Like, Post, Profile, User } = require("./index");
 const { db } = require('./db/connection.js');
+const { sequelize } = require('./db/connection.js');
 
 describe('Social Sequelzie Test', () => {
     /**
@@ -16,7 +17,51 @@ describe('Social Sequelzie Test', () => {
     test("replace with your test", function() {
         expect(true).toBe(true);
     })
+    describe('Social Sequelize Test', () => {
+        beforeAll(async () => {
+            await sequelize.sync({ force: true });
+        });
 
+        test('User has one Profile', async () => {
+            expect(User.associations.Profile).toBeDefined();
+            expect(User.associations.Profile.associationType).toBe('HasOne');
+        });
+
+        test('Profile belongs to User', async () => {
+            expect(Profile.associations.User).toBeDefined();
+            expect(Profile.associations.User.associationType).toBe('BelongsTo');
+        });
+
+        test('User has many Posts', async () => {
+            expect(User.associations.Posts).toBeDefined();
+            expect(User.associations.Posts.associationType).toBe('HasMany');
+        });
+
+        test('Post has one User', async () => {
+            expect(Post.associations.User).toBeDefined();
+            expect(Post.associations.User.associationType).toBe('HasOne');
+        });
+
+        test('Post has many Comments', async () => {
+            expect(Post.associations.Comments).toBeDefined();
+            expect(Post.associations.Comments.associationType).toBe('HasMany');
+        });
+
+        test('Comment belongs to Post', async () => {
+            expect(Comment.associations.Post).toBeDefined();
+            expect(Comment.associations.Post.associationType).toBe('BelongsTo');
+        });
+
+        test('User has many Likes', async () => {
+            expect(User.associations.Likes).toBeDefined();
+            expect(User.associations.Likes.associationType).toBe('HasMany');
+        });
+
+        test('Like has many Users', async () => {
+            expect(Like.associations.Users).toBeDefined();
+            expect(Like.associations.Users.associationType).toBe('HasMany');
+        });
+    });
 
 
 
